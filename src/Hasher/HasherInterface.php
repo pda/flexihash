5b13d7a5a5af6a2acf5e48055b8a09f770b9f5f2
Koji Kobayashi <kojik1010@gmail.com>
2016-04-22 12:43:16 -0700

    Fix Md5Hasher. fixes #13

--- src/Hasher/HasherInterface.php
+++ src/Hasher/HasherInterface.php
@@ -13,9 +13,6 @@ interface HasherInterface
     /**
      * Hashes the given string into a 32bit address space.
      *
-     * Note that the output may be more than 32bits of raw data, for example
-     * hexidecimal characters representing a 32bit value.
-     *
      * The data must have 0xFFFFFFFF possible values, and be sortable by
      * PHP sort functions using SORT_REGULAR.
      *
