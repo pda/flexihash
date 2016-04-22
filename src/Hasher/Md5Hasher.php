b3cb3e90ca85fbdcf69265cb3f54fff704c640df
Koji Kobayashi <kojik1010@gmail.com>
2016-04-22 12:14:11 -0700

    Fix Md5Hasher. fixes #13

--- src/Hasher/Md5Hasher.php
+++ src/Hasher/Md5Hasher.php
@@ -3,7 +3,7 @@
 namespace Flexihash\Hasher;
 
 /**
- * Uses MD5 to hash a value into a 32bit binary string data address space.
+ * Uses MD5 to hash a value into a 32bit int.
  *
  * @author Paul Annesley
  * @license http://www.opensource.org/licenses/mit-license.php
@@ -12,9 +12,8 @@ class Md5Hasher implements HasherInterface
 {
     public function hash($string)
     {
-        return substr(md5($string), 0, 8); // 8 hexits = 32bit
-
-        // 4 bytes of binary md5 data could also be used, but
-        // performance seems to be the same.
+        // 8 hexits = 32bit, which also allows us to forego having to check whether it's over PHP_INT_MAX.
+        // The substring is converted to an int since hex strings sometimes get treated as ints if all digits are ints and this results in unexpected sorting order.
+        return hexdoc(substr(md5($string), 0, 8));
     }
 }
